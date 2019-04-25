82a541481df6e756bc2a2ca7bafa8b388d038e14
Leon Timmermans <fawaka@gmail.com>
2019-04-25 12:34:47 +0200

    Eliminate List::Util dependency

--- lib/Dist/Banshee/Git.pm
+++ lib/Dist/Banshee/Git.pm
@@ -8,7 +8,6 @@ our @EXPORT_OK = qw/gather_files/;
 
 use Git::Wrapper;
 use File::Slurper 'read_binary';
-use List::Util 'uniq';
 
 sub gather_files {
 	my ($self, $filter) = @_;
@@ -17,7 +16,7 @@ sub gather_files {
 	my $git = Git::Wrapper->new('.');
  
 	# Loop over files reported by git ls-files
-	my @filenames = grep { !-d } uniq $git->ls_files;
+	my @filenames = grep { !-d } $git->ls_files;
 	@filenames = grep { $filter->($_) } @filenames if $filter;
 
 	my %ret = map { $_ => read_binary($_) } @filenames;
